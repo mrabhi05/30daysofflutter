87a76ff7b5407769208f72dfeaa367515e586777
Abhishek Nair <38916532+mrabhi05@users.noreply.github.com>
2021-02-11 22:22:03 +0530

    Day 10 Done

---
+++ flutterapp/lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [Item(
+  id: "Codepur001",
+  name: "IPhone 12 Pro",
+  desc: "Apple Iphone",
+  price: 999,
+  color: "#33505a",
+  image: "https://specials-images.forbesimg.com/imageserve/5f88d723cc1266cfe615c9d3/960x0.jpg?fit=scale"
+
+)];
