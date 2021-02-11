7f3963b4405f581e7a40cc2cbec6656e5e46ccd1
Abhishek Nair <38916532+mrabhi05@users.noreply.github.com>
2021-02-11 23:00:04 +0530

    Day 11 Completed

--- flutterapp/lib/models/catalog.dart
+++ flutterapp/lib/models/catalog.dart
@@ -9,12 +9,13 @@ class Item {
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
 
-final products = [Item(
-  id: "Codepur001",
-  name: "IPhone 12 Pro",
-  desc: "Apple Iphone",
-  price: 999,
-  color: "#33505a",
-  image: "https://specials-images.forbesimg.com/imageserve/5f88d723cc1266cfe615c9d3/960x0.jpg?fit=scale"
-
-)];
+final products = [
+  Item(
+      id: "Codepur001",
+      name: "IPhone 12 Pro",
+      desc: "Apple Iphone",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://specials-images.forbesimg.com/imageserve/5f88d723cc1266cfe615c9d3/960x0.jpg?fit=scale")
+];
