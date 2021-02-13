791740928f1f2b61527011d1727c30dc73acf2ae
Abhishek Nair <38916532+mrabhi05@users.noreply.github.com>
2021-02-13 23:07:58 +0530

    Day 13 Completed

--- flutterapp/lib/pages/home_page.dart
+++ flutterapp/lib/pages/home_page.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterapp/models/catalog.dart';
 import 'package:flutterapp/widgets/drawer.dart';
 import 'package:flutterapp/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Abhishek";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    
+  }
 
   @override
   Widget build(BuildContext context) {
